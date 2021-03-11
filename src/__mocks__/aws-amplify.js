60a95acc8e6c4ca476f48bc6303a264e467529fa
Mark Grebe <grebe@ucmo.edu>
2021-03-11 14:10:43 -0600

    Update list test, and addded Edit and Add tests

--- src/__mocks__/aws-amplify.js
+++ src/__mocks__/aws-amplify.js
@@ -1,32 +1,89 @@
-import { listCars } from '../graphql/queries'
+import { listCars }  from '../graphql/queries'
+import { getCar }    from '../graphql/queries'
+import { createCar } from '../graphql/mutations'
 import { deleteCar } from '../graphql/mutations'
+import { updateCar } from '../graphql/mutations'
 
-const carList = {
-    data: {
-        listCars: {
-            items: [
-                    { id:1, make:"Ford",  model:"Mustang", year:1998 },
-                    { id:2, make:"Honda", model:"CRV",     year:2018 }
-                   ]
-        }
-    }
-};
+// Test variables to make sure that API has been called and with the input
+// values we expect.
+export let listCount = 0;
+export let getCount = 0;
+export let deleteCount = 0;
+export let addCount = 0;
+export let editCount = 0;
+export let addInput = undefined;
+export let editInput = undefined;
 
-const deleteList = {
-    data: {
-        deleteCar: { id:1,  make:"Ford",  model:"Mustang", year:1998}
-    }
-};
-
+// Mock the aws-ampligy API graphql function.
+// Based on the query type, it will return a Promise that resolves to the
+// object we expect to be returned from the API base on our test
 export const API = {
-    graphql: async (query) => {
-        if (query === listCars)
+    graphql: async (op) => {
+        if (op.query === listCars) {
+            listCount += 1;
+            const carList = {
+                data: {
+                    listCars: {
+                        items: [
+                                { id:1, make:"Ford",  model:"Mustang", year:1998 },
+                                { id:2, make:"Honda", model:"CRV",     year:2018 }
+                               ]
+                    }
+                }
+            };
             return new Promise((resolve) => { resolve(carList) });
-        else if (query === deleteCar)
+        }
+        else if (op.query === getCar) {
+            getCount += 1;
+            const getList = {
+                data: {
+                    getCar: { id:1,  make:"Ford",  model:"Mustang", year:1998 }
+                }
+            };
+            return new Promise((resolve) => { resolve(getList) });
+        }
+        else if (op.query === deleteCar) {
+            deleteCount += 1;
+            const deleteList = {
+                data: {
+                    deleteCar: { id:1,  make:"Ford",  model:"Mustang", year:1998}
+                }
+            };
             return new Promise((resolve) => { resolve(deleteList) });
-    }
+        }
+        else if (op.query === createCar) {
+            addCount += 1;
+            addInput = op.variables.input;
+            const addList = {
+                    data: {
+                        createCar: { id:3,  
+                                     make:op.variables.make,
+                                     model:op.variables.model, 
+                                     year:op.variables.year}
+                    }
+                };
+            return new Promise((resolve) => { resolve(addList) });
+            }
+        else if (op.query === updateCar) {
+            editCount += 1;
+            editInput = op.variables.input;
+            const editList = {
+                    data: {
+                        updateCar: { id:op.variables.id,  
+                                     make:op.variables.make,
+                                     model:op.variables.model, 
+                                     year:op.variables.year}
+                    }
+                };
+            return new Promise((resolve) => { resolve(editList) });
+            }
+        }
 };
 
-export const graphqlOperation = (x) => {
-    return x;
+// Mock the graphqlOperation and simply return the input parameters
+// as an object we know that the Component under test will pass to the
+// API.graphql function.
+export const graphqlOperation = (query, input) => {
+      return {query: query, variables: input}
 }
+
--- src/components/ListComponent.test.js
+++ src/components/ListComponent.test.js
@@ -1,41 +1,47 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import ListComponent from './ListComponent'
-import { API, graphqlOperation } from 'aws-amplify';
+import { API, graphqlOperation, listCount, deleteCount } from 'aws-amplify';
 import { listCars } from '../graphql/queries'
 import { deleteCar } from '../graphql/mutations'
 
 jest.mock('aws-amplify');
 
-describe('My test test suite', () => { 
+describe('Example test suite', () => { 
     it('`true` should be `true`', () => {
         expect(true).toBe(true); 
     });
     it('`false` should be `false`', () => { 
         expect(false).toBe(false);
     }); 
-    it('`1` should be `1`', () => { 
-        expect(1).toBe(1);
-    }); 
 });
 
 describe('List Component Test Suite', () => { 
-    it('List Component Test', async () => {
+    it('List Component Display Test', async () => {
+        // Render the component with React Test Library,
         render(<ListComponent/>);
-        await waitFor(() => expect(screen.getByText("Ford")).toBeInTheDocument);
+        // Wait for the mocked API call to finish
+        await waitFor(() => expect(listCount).toBe(1));
+        // Make sure the two cars we expect to be in the table are there.
+        expect(screen.getByText("Ford")).toBeInTheDocument;
         expect(screen.getByText("Mustang")).toBeInTheDocument;
         expect(screen.getByText("1998")).toBeInTheDocument;
         expect(screen.getByText("Honda")).toBeInTheDocument;
         expect(screen.getByText("CRV")).toBeInTheDocument;
         expect(screen.getByText("2018")).toBeInTheDocument;
-        //expect(API).toHaveBeenCalledWith(listCars);
     });
     it('List Component Delete Test', async () => {
+        // Render the component with React Test Library,
         render(<ListComponent/>);
-        await waitFor(() => expect(screen.getByText("Ford")).toBeInTheDocument);
+        // Wait for the mocked API call to finish
+        await waitFor(() => expect(listCount).toBe(2));
+        // Click the delete button for the first row.
         fireEvent.click(screen.getAllByText('Delete')[0])
-        expect(screen.getByText("Ford")).toNotBeInTheDocument;
-        expect(screen.getByText("Mustang")).toNotBeInTheDocument;
-        expect(screen.getByText("1998")).toNotBeInTheDocument;
+        // Wait for the mocked API call to finish
+        await waitFor(() => expect(deleteCount).toBe(1));
+        // Make sure the first car was deleted and the second car is still there
+        expect(screen.queryAllByText("Ford")).toStrictEqual([]);
+        expect(screen.queryAllByText("Mustang")).toStrictEqual([]);
+        expect(screen.queryAllByText("1998")).toStrictEqual([]);
         expect(screen.getByText("Honda")).toBeInTheDocument;
         expect(screen.getByText("CRV")).toBeInTheDocument;
         expect(screen.getByText("2018")).toBeInTheDocument;
